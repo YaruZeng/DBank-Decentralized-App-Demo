00726e7c98fa9ac6096b51c1933a696ff8ebba89
IvyZayn <kakapon713@outlook.com>
2023-08-30 17:07:56 +0800

    Add backend logic with Motoko and construct a frontend with JavaScript

--- src/dbank/main.mo
+++ src/dbank/main.mo
@@ -1,5 +1,41 @@
-actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug";
+import Nat "mo:base/Nat";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+actor DBank { // create a class
+  stable var currentValue: Float = 300.0; // create a variable with default as a natural number on base that a positive number is assigned to it
+  // currentValue := 300;
+  // Debug.print(debug_show(currentValue));
+
+  stable var startTime = Time.now();
+  // startTime := Time.now();
+
+  public func topUp(amount: Float) { // create a update function; public: make the function can be called outside this canister
+    currentValue += amount;
+    // Debug.print(debug_show(currentValue));
   };
-};
+  
+  public func withDraw(amount: Float) {
+    let tempValue: Float = currentValue - amount; // create a constant whose type is int
+    if (tempValue >= 0) { // substract the amount only when the final value >= 0
+      currentValue -= amount;
+      // Debug.print(debug_show(currentValue));
+    } else {
+      Debug.print("Amount too large, currentValue less than zero. ");
+    }
+  };
+
+  public query func checkBalance(): async Float { // create a query function (whose speed is faster than update function goes into blockchain)
+    return currentValue;
+  };
+
+  public func compound() {
+    let currentTime = Time.now();
+    let timeElapsedNS = currentTime - startTime;
+    let timeElapsedS = timeElapsedNS / 1000000000; // convert nanosecond to second
+    currentValue := currentValue * (1.01 ** Float.fromInt(timeElapsedS)); // keep all elements float
+    startTime := currentTime;
+  }
+}
+
--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,19 +1,38 @@
-import { dbank } from "../../declarations/dbank";
+import {dbank} from "../../declarations/dbank";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+window.addEventListener("load", async function () {
+  // once the page is refreshed, update the current balance through the func update()
+  update(); 
 });
+
+document.querySelector("form").addEventListener("submit", async function(event) {
+  event.preventDefault(); // prevent the default refreshing of the form
+
+  const button = event.target.querySelector("#submit-btn"); // to control the state of the button
+
+  const inputAmount = parseFloat(document.getElementById("input-amount").value); // get the topup amount from user
+  const outputAmount = parseFloat(document.getElementById("withdraw-amount").value); // get the withdraw amount from user
+
+  button.setAttribute("disabled", true); // the button turns disabled after the user hits "submit"
+
+  if (document.getElementById("input-amount").value.length != 0) {
+    await dbank.topUp(inputAmount); // topup the amount through dbank when the input value is not null
+  }
+
+  if (document.getElementById("withdraw-amount").value.length != 0) {
+    await dbank.withDraw(outputAmount); // withdraw the amount through dbank the input value is not null
+  }
+
+  await dbank.compound(); // compound every submit
+
+  update(); // update the balance 
+  document.getElementById("input-amount").value = ""; // clear the input after every update
+  document.getElementById("withdraw-amount").value = "";
+  button.removeAttribute("disabled"); // enable the button after every update
+
+});
+
+async function update() {
+  const currentAmount = await dbank.checkBalance(); // check current value from ICP
+  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100; // update the frontend balance
+};
